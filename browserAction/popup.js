8d1516ba3710c7539865d9c6dd7fe639e831072d
Natàlia <natalia.cavi@gmail.com>
2018-08-26 11:55:16 +0200

    Parametrized density threshold

--- browserAction/popup.js
+++ browserAction/popup.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const densityElement = document.getElementById('density');
+
 chrome.runtime.onMessage.addListener(({ density }) => {
-  const densityElement = document.getElementById('density');
-  densityElement.classList.add(density < 30 ? 'ok' : 'ko');
-  densityElement.innerHTML = `${density}%`;
+  chrome.storage.sync.get('densityThreshold', ({ densityThreshold }) => {
+    densityElement.classList.add(density < densityThreshold ? 'ok' : 'ko');
+    densityElement.innerHTML = `${density}%`;
+  });
 });
 
 chrome.tabs.getCurrent(tabId => chrome.tabs.executeScript(tabId, { file: 'scripts/calculateDensity.js' }));
--- options/options.js
+++ options/options.js
@@ -1,11 +1,22 @@
 'use strict';
 
-const querySelectorElement = document.getElementById('query-selector');
+function manageInput({ elementName, storageKey, defaultValue }) {
+  const element = document.getElementById(elementName);
 
-chrome.storage.sync.get('querySelector', ({ querySelector }) => {
-  querySelectorElement.value = querySelector || '';
-});
+  chrome.storage.sync.get(storageKey, (storage) => {
+    element.value = storage[storageKey] || defaultValue;
+  });
 
-querySelectorElement.onblur = (event) => {
-  chrome.storage.sync.set({ querySelector: event.srcElement.value });
-};
+  element.onblur = (event) => {
+    const element = event.srcElement;
+    const value = element.type === 'number' ? element.valueAsNumber : element.value;
+    chrome.storage.sync.set({ [storageKey]: value });
+  };
+}
+
+const inputFields = [
+  { elementName: 'query-selector', storageKey: 'querySelector', defaultValue: '' },
+  { elementName: 'density-threshold', storageKey: 'densityThreshold', defaultValue: 0 },
+];
+
+inputFields.forEach(field => manageInput(field));
